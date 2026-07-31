0aa57b447fa61cc76a19894a341aeab0cea0da28
nila <nilabmd2@gmail.com>
2026-07-31 19:24:21 +0330

    change massage and verbose name

--- farm/models.py
+++ farm/models.py
@@ -5,17 +5,17 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Farm(models.Model):
-    name=models.CharField(max_length=20,verbose_name="نام")
-    city=models.CharField(max_length=30,verbose_name="شهر")
+    name=models.CharField(max_length=20,verbose_name="name")
+    city=models.CharField(max_length=30,verbose_name="city")
     area=models.IntegerField(validators=[MinValueValidator(0)],verbose_name="مساحت")
-    descreaption=models.TextField(verbose_name="توضیحات")
+    descreaption=models.TextField(verbose_name="description")
     phone=models.CharField(max_length=11,validators=[
         RegexValidator(
             regex=r'^0\d{10}$',  # 0 + 10 رقم
-            message='شماره تلفن باید با 0 شروع شود و 11 رقم باشد'
+            message='The phone number must start with 0 and be 11 digits long.'
         )
     ]
-    ,verbose_name='شماره موبایل')
+    ,verbose_name='phone number')
     owner=models.ForeignKey(User,on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
